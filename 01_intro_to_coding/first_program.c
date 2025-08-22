6fb95c724de961d1256f1df47b1632684a43f1b2
Tmckinley08 <tmckinley3362@floridapoly.edu>
2025-08-22 14:36:34 +0000

    Created the first program

---
+++ 01_intro_to_coding/first_program.c
@@ -0,0 +1,24 @@
+/*
+
+milti-line comment
+08/20/2025
+Tyler McKinley
+the first program in c
+
+*/
+
+// One line comment 
+
+// Preprocessor directive 
+// stdio.h - standard input output
+// . h - header file 
+#include <stdio.h>    
+
+// Create the main function 
+int main(void) {
+    printf("Hello\nWorld\n"); // \n (new line in the terminal) - escape sequence
+
+    printf("I\nam\nready\nto\ncode!\n");
+
+    return 0;
+}
